8484384a59cad363a13cf5d0cd6ecee1bf2c1606
badou300 <148993241+badou300@users.noreply.github.com>
2023-12-18 17:56:48 +0100

    Update filter-find.js

--- badou 2/filter-find.js
+++ badou 2/filter-find.js
@@ -1,134 +1,85 @@
+const books = require('./library.json');
 
-const livres = require('./library.json');
-
-/**
- * @param {array} stock 
- * @returns {number} 
- */
 function nbLivres(stock) {
   return stock.length;
 }
 
-/**
- * @param {array} stock 
- */
 function listerTousLesTitres(stock) {
-  stock.forEach((livre) => {
-    console.log(livre.title);
+  stock.forEach((book) => {
+    console.log(book.title);
   });
 }
 
-/**
- * @param {array} stock 
- * @param {string} titre
- * @param {string} auteur 
- * @param {string} genre
- * @param {number} pages 
- * @param {string} langue
- * @param {number} annee 
- * @returns {array} 
- */
-function ajouterLivre(stock, titre, auteur, genre, pages, langue, annee) {
+function ajouterLivre(stock, title, author, genre, pages, language, year) {
   const nouveauLivre = {
-    stock,
-    auteur,
-    titre,
-    pages,
+    title,
+    author,
     genre,
-    langue,
-    annee
+    pages,
+    language,
+    year
   };
   stock.push(nouveauLivre);
   return stock;
 }
 
-/**
- * @param {array} stock 
- * @param {number} index
- */
 function supprimerLivre(stock, index) {
   stock.splice(index, 1);
 }
 
-/**
- * @param {array} stock
- * @param {number} index 
- * @param {string} nouveauTitre 
- */
-function mettreAJourTitre(stock, index, nouveauTitre) {
-  stock[index].titre = nouveauTitre;
+function mettreAJourTitre(stock, index, newTitle) {
+  stock[index].title = newTitle;
 }
 
-/**
- * @param {array} stock 
- */
 function listerLivresEnAnglais(stock) {
-  const livresEnAnglais = stock.filter((livre) => livre.langue === 'English');
+  const livresEnAnglais = stock.filter((book) => book.language === 'English');
   listerTousLesTitres(livresEnAnglais);
 }
 
-/**
- * @param {array} stock 
- */
 function listerLivresPlusDe300Pages(stock) {
-  const livresPlusDe300Pages = stock.filter((livre) => livre.pages > 300);
+  const livresPlusDe300Pages = stock.filter((book) => book.pages > 300);
   listerTousLesTitres(livresPlusDe300Pages);
 }
 
-/**
- * @param {array} stock 
- * @returns {array} 
- */
 function livresEntre2000Et2010(stock) {
-  return stock.filter((livre) => livre.annee > 2000 && livre.annee <= 2010);
+  return stock.filter((book) => book.year > 2000 && book.year <= 2010);
 }
 
-/**
- * @param {array} stock
- * @returns {array} 
- */
 function livresFantasyEnAnglais(stock) {
-  return stock.filter((livre) => livre.genre === 'Fantasy' && livre.langue === 'English');
+  return stock.filter((book) => book.genre === 'Fantasy' && book.language === 'English');
 }
 
-/**
- * @param {array} stock 
- * @returns {object}
- */
-function livreContenantRide(stock) {
-  return stock.find((livre) => livre.titre.toLowerCase().includes('ride'));
+function bookContainingRide(stock) {
+  return stock.find((book) => book.title.toLowerCase().includes('ride'));
 }
 
-const stock = livres.slice();
+console.log("Nombre de livres:", nbLivres(books));
 
-console.log('Nombre de livres:', nbLivres(stock));
+console.log("Tous les titres-------------------:");
+listerTousLesTitres(books);
 
-console.log('\nListe de tous les titres:');
-listerTousLesTitres(stock);
+console.log("Ajout d'un livre------------------:");
+ajouterLivre(books, 'badou', 'badouride', 'Fantasy', 300, 'English', 2002);
+listerTousLesTitres(books);
 
-console.log('\nAjouter un nouveau livre:');
-const stockMaj = ajouterLivre(stock, ' jeux ride', 'badou', 'Genre', 300, 'English', 2002);
-console.log('Stock mis à jour:', stockMaj);
+console.log("Suppression d'un livre------------:");
+supprimerLivre(books, 0);
+listerTousLesTitres(books);
 
-console.log('\nSupprimer un livre à l\'index 1:');
-supprimerLivre(stockMaj, 1);
-console.log('Stock mis à jour après suppression:', stockMaj);
+console.log("Mise à jour du titre--------------:");
+mettreAJourTitre(books, 0, "Nouveau Titre");
+listerTousLesTitres(books);
 
-console.log('\nMettre à jour le titre du livre à l\'index 0:');
-mettreAJourTitre(stockMaj, 0, 'Titre mis à jour');
-console.log('Stock mis à jour après mise à jour du titre:', stockMaj);
+console.log("Livres en anglais-----------------:");
+listerLivresEnAnglais(books);
 
-console.log('\nListe des livres en anglais:');
-listerLivresEnAnglais(stockMaj);
+console.log("Livres de plus de 300 pages-------:");
+listerLivresPlusDe300Pages(books);
 
-console.log('\nListe des livres avec plus de 300 pages:');
-listerLivresPlusDe300Pages(stockMaj);
+console.log("Livres entre 2000 et 2010--------:");
+listerTousLesTitres(livresEntre2000Et2010(books));
 
-console.log('\nLivres publiés entre 2000 et 2010:');
-console.log(livresEntre2000Et2010(stockMaj));
+console.log("Livres de fantasy en anglais-----:");
+listerTousLesTitres(livresFantasyEnAnglais(books));
 
-console.log('\nLivres Fantasy en anglais:');
-console.log(livresFantasyEnAnglais(stockMaj));
-
-console.log('\nLivre contenant "ride" dans le titre:');
-console.log(livreContenantRide(stockMaj));
+console.log("Livre contenant 'ride'-----------:", bookContainingRide(books));
